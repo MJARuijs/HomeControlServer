2a5200b2421c90adf34ba226573e2ce21612cf2e
MJARuijs <mja.ruijs@gmail.com>
2018-05-08 15:02:29 +0200

    Create runnable jar

--- src/Main.kt
+++ src/Main.kt
@@ -15,8 +15,9 @@ object Main {
         val server = Server(4444)
         println("Server started")
         while (true) {
-            val client = ArduinoClient("hi", server.accept())
-            val message = client.readMessage()
+            val client = SecureClient(server.accept())
+//            val client = ArduinoClient("hi", server.accept())
+            val message = client.decodeMessage()
             println(message)
             client.writeMessage("Hello!")
 //            val client = SecureClient(server.accept())
